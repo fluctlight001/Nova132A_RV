a41837864d2d033b22f49e05d9a3bef4ada2353b
fluctlight001 <609804988@qq.com>
2025-05-05 09:57:59 +0800

    add ctrl

---
+++ ctrl.v
@@ -0,0 +1,26 @@
+`define StallBus 6
+module ctrl(
+    input wire rst_n,
+    input wire stallreq_id,
+    input wire stallreq_ex,
+    // input wire stallreq_outside,
+    output reg [`StallBus-1:0] stall
+);
+    always @ (*) begin
+        if (!rst_n) begin
+            stall = `StallBus'b0;
+        end
+        // else if (stallreq_outside) begin
+        //     stall = `StallBus'b11111111;
+        // end
+        else if (stallreq_id) begin
+            stall = `StallBus'b00001111;
+        end
+        else if (stallreq_ex) begin
+            stall = `StallBus'b00011111;
+        end
+        else begin
+            stall = `StallBus'b0;
+        end
+    end
+endmodule
